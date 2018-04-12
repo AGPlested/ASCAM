fe7b6ecb2d5fb4f0906b5430b31c91a66312f79f
Nikolai Zaki <nikolai.zaki@tutanota.de>
2018-04-12 11:12:33 +0200

    save_matlab now handles all kinds of filenames properly. replaces ' ' with '_'; saves even if directory already exists and saves in correct path. ignores any extension specified by the user and adds '.mat'

--- savedata.py
+++ savedata.py
@@ -1,25 +1,29 @@
-def save_matlab(data, filename='', dirname='./', save_piezo=True, save_command=True):
+def save_matlab(data, filename='', save_piezo=True,
+                save_command=True):
     """
-    Save the data stored in a recording object in matlab files in a directory.
+    Save the series stored in a recording object in matlab files in a directory.
     Parameters:
     data - `Recording` object
-    filename [string] - name for the directory and the files
-        -- this should be only the NAME OF THE FILE, not the path and it should not contain
-        -- stupid characters like '/' etc
+    filename [string] - name for the directory and the files this should be only
+                        the NAME OF THE FILE, not the path and it should not
+                        contain problematic characters like '/' etc
     dirname [string] - name of the directory
     save_piezo [bool] - should save the piezo data
     save_command [bool] - what about the command voltage data
-    
+
     Returns:
     True if successfully saved
     False if a directory with the given name already exists
     """
-    
+    return_status = False
     # if no filename is specified we use the name of the file that was loaded
     if not filename:
-        filename = data.filename+'_SAVE'
-        
+        filename = './'+data.filename+'_SAVE'
+
+    # this next bit of code is supposed to
     N = len(filename)
+    period = False
+    slash = False
     for i, char in enumerate(filename[::-1]):
         # loop over the full filename (which includes directory) backwards
         # to extract the extension and name of the file
@@ -28,42 +32,52 @@ def save_matlab(data, filename='', dirname='./', save_piezo=True, save_command=T
         if char=='/':
             slash = N-i
             break
-    filename = filename[slash:period-1]
-    filename = ''.join(filename.split())
-    
-    # create a direcotry in which to save the data
-    dirname+=filename
-    dirname+='/'
-    try: 
-        os.mkdir(dirname)
-    except FileExistsError:
+    # this if statement seperates the variable `filename` into the full path in `dirname`
+    # and keeps everything after the last slash except the extension to name the
+    # actual saved files in the directory
+    if period and slash:
+        dirname = filename[:period-1]
+        filename = filename[slash:period-1]
+    elif slash and not period:
+        dirname = filename
+        filename = filename[slash:]
+    # replace whitespaces in dirname and filename and with underscore
+    dirname = '_'.join(dirname.split())
+    filename = '_'.join(filename.split())
+
+    try:
+        os.makedirs(dirname)
+    except OSError:
         # mkdir cannot create a directory if it already exists
         # if this happens do not save anything
-        print("""Data not saved!! \n A directory by that name already exists.
-            \n Please choose a new name.""")
-        return False
-    # save each series seperatly
-    for datakey, series in data.items():
-        file_to_save = dirname+filename+'_'+datakey+'.mat'
-        #dictionary for saving a series to matlab
-        savedict = {}
-        savedict['time'] = raw[0]['time']
-        # need this to add leading zeros to episode number in filename
-        no_episodes = len(series)
-        fill_length = len(str(no_episodes))
-        #fill savedict
-        for episode in series:
-            for name, value in episode.items():
-                if name == 'time':
-                    continue
-                elif name == 'piezo' and not save_piezo:
-                    continue
-                elif name == 'command' and not save_command:
-                    continue
-                else:
-                    n = str(episode.nthEpisode)
-                    n = n.zfill(fill_length)
-                    savedict[name+n] = value
-        #save to file
-        io.savemat(file_to_save,savedict)
-    return True
+        print("""A directory by that name already exists, make sure everything went well.""")
+    finally:
+        # save each series seperatly
+        for datakey, series in data.items():
+            file_to_save = dirname+'/'+filename+'_'+datakey+'.mat'
+            #dictionary for saving a series to matlab
+            savedict = {}
+            savedict['time'] = series[0]['time']
+            # need this to add leading zeros to episode number in filename
+            no_episodes = len(series)
+            fill_length = len(str(no_episodes))
+            #fill savedict
+            for episode in series:
+                for name, value in episode.items():
+                    # the if statements skip the items that do not need to be
+                    # saved
+                    if name == 'time':
+                        continue
+                    elif name == 'piezo' and not save_piezo:
+                        continue
+                    elif name == 'command' and not save_command:
+                        continue
+                    else:
+                    # add data to dictionary
+                        n = str(episode.nthEpisode)
+                        n = n.zfill(fill_length)
+                        savedict[name+n] = value
+            #save to file
+            io.savemat(file_to_save,savedict)
+            return_status = True
+    return return_status
