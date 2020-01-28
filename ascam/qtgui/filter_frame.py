15228e56354d01785ec1473607e9e9391cc695c4
Nikolai Zaki <kol@posteo.de>
2020-01-28 18:39:02 +0100

    first steps for gaussian filterframe

--- ascam/qtgui/filter_frame.py
+++ ascam/qtgui/filter_frame.py
@@ -1,5 +1,16 @@
-from PySide2.QtWidgets import QDialog
+from PySide2.QtWidgets import QDialog, QLineEdit, QVBoxLayout
 
 
 class FilterFrame(QDialog):
-    pass
+
+    def __init__(self, main):
+        super().__init__()
+        self.setWindowTitle("Filter") 
+        layout = QVBoxLayout()
+
+        freq_entry = QLineEdit("1000")
+
+        self.setLayout(layout)
+        layout.addWidget(freq_entry)
+        self.exec_()
+
