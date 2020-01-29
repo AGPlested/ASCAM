b0e5b2355a31e5e10aa50fe85f607b0ea4142508
Nikolai Zaki <kol@posteo.de>
2020-01-29 11:33:17 +0100

    add button for adding tabs

--- ascam/qtgui/idealization_frame.py
+++ ascam/qtgui/idealization_frame.py
@@ -1,5 +1,10 @@
+import logging
+
 from PySide2.QtWidgets import ( QTabWidget, QWidget, QVBoxLayout, QHBoxLayout,
 QPushButton, QLabel)
+
+
+debug_logger = logging.getLogger("ascam.debug")
 
 
 class IdealizationFrame(QTabWidget):
@@ -7,16 +12,34 @@ class IdealizationFrame(QTabWidget):
     def __init__(self):
         super(IdealizationFrame, self).__init__()
 
-        self.tabs = [QWidget()]
-        self.tab_init(self.tabs[0])
+        self.tabs = [IdealizationTab(self)]
+        self.addTab(self.tabs[0], "1")
 
-    def tab_init(self, tab, title=None):
-        if title is None:
-            title = str(len(self.tabs))
-        self.addTab(tab, title)
+        new_tab_button = QWidget()
+        self.addTab(new_tab_button, "+")
+
+        self.tabBarClicked.connect(self.new_tab)
+
+    def new_tab(self, index):
+        if index == len(self.tabs):
+            self.add_tab()
+
+    def add_tab(self):
+        title = str(len(self.tabs)+1)
+        debug_logger.debug(f"adding new tab with number {title}")
+        tab = IdealizationTab(self)
+        self.tabs.append(tab)
+        self.insertTab(len(self.tabs)-1, tab, title)
+
+
+class IdealizationTab(QWidget):
+
+    def __init__(self, parent):
+        super().__init__()
         layout = QVBoxLayout()
-        tab.setLayout(layout)
+        self.setLayout(layout)
         layout.addWidget(QLabel("Idealize me baby"))
         button = QPushButton("bye")
         button.clicked.connect(lambda *args: self.close())
         layout.addWidget(button)
+        
