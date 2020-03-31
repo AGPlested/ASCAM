449e06c350b01d18b348ee67783d9aee52752b07
Nikolai Zaki <kol@posteo.de>
2020-03-31 20:02:17 +0200

    first draft histogram config dialog

--- ascam/utils/widgets.py
+++ ascam/utils/widgets.py
@@ -1,5 +1,5 @@
 from PySide2 import QtGui, QtCore
-from PySide2.QtWidgets import QTextEdit
+from PySide2.QtWidgets import QTextEdit, QDialog, QVBoxLayout, QHBoxLayout
 import pyqtgraph as pg
 
 
@@ -24,19 +24,19 @@ class CustomViewBox(pg.ViewBox):
         super().__init__()
         self.exportDialog = None
         self.menu = None # Override pyqtgraph ViewBoxMenu
-        self.menu = self.getMenu() # Create the menu
+        self.menu = self.get_menu() # Create the menu
 
     def raiseContextMenu(self, ev):
         if not self.menuEnabled():
             return
-        menu = self.getMenu()
+        menu = self.get_menu()
         menu = self.scene().addParentContextMenus(self, menu, ev)
         menu.removeAction(menu.actions()[-2])  # remove the "Plot Options" menu item
 
         pos  = ev.screenPos()
         menu.popup(QtCore.QPoint(pos.x(), pos.y()))
 
-    def getMenu(self):
+    def get_menu(self):
         if self.menu is None:
             self.menu = QtGui.QMenu()
             self.viewAll = QtGui.QAction("View All", self.menu)
@@ -58,4 +58,57 @@ class CustomViewBox(pg.ViewBox):
             # self.menu.addMenu(self.leftMenu)
             # self.menu.addSeparator()
         return self.menu
+
+class EventHistConfig(QDialog):
+    def __init__(self, parent):
+        super.__init__()
+        self.parent = parent
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+    def create_widgets(self):
+        row = QHBoxLayout()
+        label = QLabel("Number of bins:")
+        row.addWidget(label)
+        self.n_bins = QLineEdit(self.parent)
+        row.addWidget(self.n_bins)
+
+        row = QHBoxLayout()
+        label = QLabel('Binning Formula')
+        row.addWidget(label)
+        self.n_bins = QLineEdit(self.parent)
+        row.addWidget(self.n_bins)
+
+        row = QHBoxLayout()
+        label = QLabel('Time Unit')
+        row.addWidget(label)
+        self.time_unit = QComboBox()
+        self.time_unit.addItems(TIME_UNIT_FACTORS.keys())
+        row.addWidget(self.time_unit)
+
+        row = QHBoxLayout()
+        label = QLabel('Time Transform')
+        row.addWidget(label)
+        # self.time_unit = QComboBox()
+        # self.time_unit.addItems(TIME_UNIT_FACTORS.keys())
+        # row.addWidget(self.time_unit)
+
+        row = QHBoxLayout()
+        label = QLabel('Count Transform')
+        row.addWidget(label)
+        # self.time_unit = QComboBox()
+        # self.time_unit.addItems(TIME_UNIT_FACTORS.keys())
+        # row.addWidget(self.time_unit)
+
+        row = QHBoxLayout()
+        ok_button = QPushButton("OK")
+        ok_button.clicked.connect(self.ok_click)
+        row.addWidget(ok_button)
+        cancel_button = QPushButton("cancel")
+        cancel_button.clicked.connect(self.close)
+        row.addWidget(cancel_button)
+
+    def ok_click(self):
+        # do histogram magic here
+        self.close()
 
