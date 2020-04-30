eb9b216aca454153035479a3dc819fda662be9c4
!whoami <kol@posteo.de>
2020-04-30 19:20:45 +0200

    avoid importing other modules in constants

--- ascam/constants.py
+++ ascam/constants.py
@@ -1,15 +1,8 @@
-import os 
-
-from ascam.utils import get_install_path
-
-
 DEFAULT_GAUSS_CUTOFF_FREQ = 1000
 
 CURRENT_UNIT_FACTORS = {"fA": 1e15, "pA": 1e12, "nA": 1e9, "µA": 1e6, "mA": 1e3, "A": 1}
 VOLTAGE_UNIT_FACTORS = {"uV": 1e6, "mV": 1e3, "V": 1}
 TIME_UNIT_FACTORS = {"us": 1e6, "ms": 1e3, "s": 1}
 
-INSTALL_PATH = get_install_path()
-TEST_FILE_NAME = os.path.join(INSTALL_PATH, "data/GluA2_T1_SC-recording_40kHzSR.mat")
 VERSION = 0.1
 
