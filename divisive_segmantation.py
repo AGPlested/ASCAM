3f1fae876db4216d6d34f9181be2844a66fb93e1
Nikolai <kol@posteo.de>
2022-11-16 12:48:22 +0100

    remove/change some comments

--- divisive_segmantation.py
+++ divisive_segmantation.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# It looks like the t-test could be computed in a vectorized manner
+# for all point in parallel.
 def tTestCP(data, noise_std):
     """
         Input :
@@ -12,17 +14,12 @@ def tTestCP(data, noise_std):
     # Speed up T-Test computations by computing all sums only once
     cum_sum = np.cumsum(data)
     total_sum = cum_sum[-1]
-    # main loop
-    # Drop first and last data points from the loop
-    # (can't compute a mean with only 1 data point)
+    # Drop first and last data points from the loop as in the original
+    # author's code.
     for n in range(2,N-1):
-        # compute mean of segment 1 assuming CP at k mean(data(1:CP))
         mu_1 = cum_sum[n] / n
-        # compute mean of segment 2 assuming CP at k mean(data(1+CP:end))
         mu_2 = (total_sum - cum_sum[n]) / (N-n)
-        # compute t-value
         t = abs((mu_2 - mu_1)) / noise_std / np.sqrt(1/n+1/(N-n))
-        # is the new t value better than the current best T value?
         if t > T:
             T = t      # best T value so far
             CP = n     # Location of best T value
