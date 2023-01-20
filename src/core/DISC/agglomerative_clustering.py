fix wrong computation of Ward distances

src/core/DISC/agglomerative_clustering.py
@@ -27,7 +27,7 @@ def Ward_distances(data_fit):
     for i in range(K):
         for j in range(i+1, K):
             ward_d[i,j] = (np.sqrt( 2*n[i]*n[j]/(n[i]+n[j]) )
-                           * np.sqrt(states[i]**2+states[j]**2)
+                           * np.sqrt((states[i]-states[j])**2)
                            )
     return ward_d
 