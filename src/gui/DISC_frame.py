16c2571dc3264d93ad19ccb0af3fd69ecde04ced
Nikolai <kol@posteo.de>
2023-02-16 09:36:10 +0100

    fixup! move disc frame to its own file

--- src/gui/DISC_frame.py
+++ src/gui/DISC_frame.py
@@ -1,21 +1,20 @@
 import logging
 
-from PySide2 import QtCore
+# from PySide2 import QtCore
 from PySide2.QtWidgets import (
     QLabel,
-    QComboBox,
-    QCheckBox,
+    QWidget,
+    QRadioButton,
     QLineEdit,
-    QToolButton,
     QPushButton,
+    QVBoxLayout
 )
 
-from ..gui import ExportFADialog
-from ..utils.widgets import EntryWidget, TableFrame
-from ..constants import CURRENT_UNIT_FACTORS
-from ..constants import CURRENT_UNIT_FACTORS, ANALYSIS_FRAME_WIDTH
+from ..utils.widgets import EntryWidget
+from ..constants import ANALYSIS_FRAME_WIDTH
 
 debug_logger = logging.getLogger("ascam.debug")
+
 
 class DISCFrame(QWidget):
     def __init__(self, main):
@@ -30,14 +29,16 @@ class DISCFrame(QWidget):
 
         # self.main.ep_frame.ep_list.currentItemChanged.connect(self.on_episode_click)
 
-    # @property
-    # def current_tab(self):
-        # return self.tab_frame.currentWidget()
-
     def create_widgets(self):
-        pass
-        # self.tab_frame = IdealizationTabFrame(self)
-        # self.layout.addWidget(self.tab_frame)
+        self.bic_method_selector = QRadioButton("BIC implmentation")
+        self.layout.addWidget(self.bic_method_selector)
+
+        self.divseg_frame = DivSegFrame(self)
+        self.layout.addWidget(self.divseg_frame)
+        self.ac_frame = ACFrame(self)
+        self.layout.addWidget(self.ac_frame)
+        self.viterbi_frame = ViterbiFrame(self)
+        self.layout.addWidget(self.viterbi_frame)
 
         # self.calc_button = QPushButton("Calculate idealization")
         # self.calc_button.clicked.connect(self.calculate_click)
@@ -65,13 +66,7 @@ class DISCFrame(QWidget):
         # self.layout.addStretch()
 
     # def on_episode_click(self, item, *args):
-        # self.idealize_episode()
-
-    # def close_tab(self):
-        # if self.tab_frame.count() > 2:
-        #     self.tab_frame.removeTab(self.tab_frame.currentIndex())
-        # else:
-        #     self.close_frame()
+    #     self.idealize_episode()
 
     def close_frame(self):
         # self.main.ep_frame.ep_list.currentItemChanged.disconnect(self.on_episode_click)
@@ -79,84 +74,59 @@ class DISCFrame(QWidget):
         self.main.plot_frame.update_plots()
         self.close()
 
-    # def create_histogram_frame(self):
-        # self.current_tab.create_histogram_frame()
+class DivSegFrame(EntryWidget):
+    def __init__(self, main):
+        self.main = main
+        super().__init__(main)
 
-    # def create_event_frame(self):
-        # self.current_tab.create_event_frame()
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
 
-    # def export_events(self):
-        # self.get_params()
-        # self.current_tab.idealization_cache.get_events()
-        # filename = QFileDialog.getSaveFileName(
-        #     self, dir=self.main.filename[:-4] + "_events.csv", filter="*.csv"
-        # )[0]
-        # self.current_tab.idealization_cache.export_events(
-        #     filename, self.current_tab.time_unit, self.current_tab.trace_unit
-        # )
+        self.div_seg_label = QLabel("Divisive segmentation")
+        self.layout.addWidget(self.div_seg_label)
 
-    # def export_idealization(self):
-        # self.get_params()
-        # self.idealize_series()
-        # ExportIdealizationDialog(self.main, self.current_tab.idealization_cache)
-        # # filename = QFileDialog.getSaveFileName(
-        # #     self, dir=self.main.filename[:-4] + "_idealization.csv", filter="*.csv"
-        # # )[0]
-        # # self.current_tab.idealization_cache.export_idealization(
-        # #     filename,
-        # #     self.current_tab.time_unit,
-        # #     self.current_tab.trace_unit,
-        # # )
+        self.div_seg_button = QPushButton("Divide & Segment")
+        self.div_seg_button.clicked.connect(self.div_seg)
+        self.layout.addWidget(self.div_seg_button)
 
-    # def get_params(self):
-        # return self.current_tab.get_params()
+        self.close_button = QPushButton("Close")
+        self.close_button.clicked.connect(self.close_frame)
+        self.layout.addWidget(self.close_button)
 
-    # def idealization(self, n_episode=None):
-        # if self.current_tab.idealization_cache is not None:
-        #     return self.current_tab.idealization_cache.idealization(n_episode)
-        # return None
+class ACFrame(EntryWidget):
+    def __init__(self, main):
+        self.main = main
+        super().__init__(main)
 
-    # def time(self, n_episode=None):
-        # return self.current_tab.idealization_cache.time(n_episode)
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
 
-    # def calculate_click(self):
-        # self.get_params()
-        # self.idealize_episode()
-        # self.main.plot_frame.update_episode()
+        self.div_seg_label = QLabel("Divisive segmentation")
+        self.layout.addWidget(self.div_seg_label)
 
-    # def idealize_episode(self):
-        # self.current_tab.idealization_cache.idealize_episode()
+        self.div_seg_button = QPushButton("Divide & Segment")
+        self.div_seg_button.clicked.connect(self.div_seg)
+        self.layout.addWidget(self.div_seg_button)
 
-    # def idealize_series(self):
-        # self.current_tab.idealization_cache.idealize_series()
+        self.close_button = QPushButton("Close")
+        self.close_button.clicked.connect(self.close_frame)
+        self.layout.addWidget(self.close_button)
 
-    # def track_cursor(self, y_pos):
-        # """Track the position of the mouse cursor over the plot and if mouse 1
-        # is pressed adjust the nearest threshold/amplitude line by dragging the
-        # cursor."""
+class ViterbiFrame(EntryWidget):
+    def __init__(self, main):
+        self.main = main
+        super().__init__(main)
 
-        # amps, thetas = self.get_params()[:2]
-        # if thetas.size > 0:
-        #     tc_diff = np.min(np.abs(thetas - y_pos))
-        # else:
-        #     tc_diff = np.inf
-        # if amps.size > 0:
-        #     amp_diff = np.min(np.abs(amps - y_pos))
-        # else:
-        #     amp_diff = np.inf
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
 
-        # y_pos *= CURRENT_UNIT_FACTORS[self.current_tab.trace_unit]
-        # if self.current_tab.neg_check.isChecked():
-        #     y_pos *= -1
-        # if tc_diff < amp_diff and self.current_tab.show_threshold_check.isChecked():
-        #     new_str = update_number_in_string(
-        #         y_pos, self.current_tab.threshold_entry.toPlainText()
-        #     )
-        #     self.current_tab.threshold_entry.setPlainText(new_str)
-        #     self.current_tab.auto_thresholds.setChecked(False)
-        # elif self.current_tab.show_amp_check.isChecked():
-        #     new_str = update_number_in_string(
-        #         y_pos, self.current_tab.amp_entry.toPlainText()
-        #     )
-        #     self.current_tab.amp_entry.setPlainText(new_str)
-        # self.calculate_click()
+        self.div_seg_label = QLabel("Divisive segmentation")
+        self.layout.addWidget(self.div_seg_label)
+
+        self.div_seg_button = QPushButton("Divide & Segment")
+        self.div_seg_button.clicked.connect(self.div_seg)
+        self.layout.addWidget(self.div_seg_button)
+
+        self.close_button = QPushButton("Close")
+        self.close_button.clicked.connect(self.close_frame)
+        self.layout.addWidget(self.close_button)
