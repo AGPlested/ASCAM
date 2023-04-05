78cc5ca542f7cdb12785f8fdd5a3d8dfd1fd1a8a
Nikolai <kol@posteo.de>
2023-04-05 16:28:23 +0200

    when plotting idealization ask forgiveness instead of permission

--- src/gui/plot_frame.py
+++ src/gui/plot_frame.py
@@ -194,14 +194,16 @@ class PlotFrame(QWidget):
         self.trace_line = self.trace_plot.plot(
             self.main.data.episode().time, self.main.data.episode().trace, pen=pen
         )
-        if (
-            self.main.tc_frame is not None
-            and self.main.tc_frame.idealization() is not None
-        ):
+        try:
             id_pen = pg.mkPen(color=ORANGE)
             self.trace_plot.plot(
-                self.main.tc_frame.time(), self.main.tc_frame.idealization(), pen=id_pen
+                self.main.id_frame.time(), self.main.id_frame.idealization(), pen=id_pen
             )
+        except AttributeError as e:
+            if "'NoneType' object has no attribute 'time'" in str(e):
+                pass
+            else:
+                raise e
         if self.show_command:
             self.command_plot.plot(
                 self.main.data.episode().time, self.main.data.episode().command, pen=pen
