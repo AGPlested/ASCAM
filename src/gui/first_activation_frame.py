9b7052df2da3bcebb550ef898af22bba310523ad
Ece Kuru <kuruece@student.hu-berlin.de>
2023-09-12 19:19:44 +0200

    Correct typos

--- src/gui/first_activation_frame.py
+++ src/gui/first_activation_frame.py
@@ -106,7 +106,7 @@ class FirstActivationFrame(EntryWidget):
         ExportFADialog(self.main)
 
     def show_first_activation_table(self):
-        debug_logger.debug("creating fist activation table")
+        debug_logger.debug("creating first activation table")
         self.main.data.detect_fa(self.threshold)
 
         self.table_frame = TableFrame(
@@ -116,7 +116,7 @@ class FirstActivationFrame(EntryWidget):
             ),
             header=[
                 "Episode Number",
-                f"First Activatime Time [self.time_unit]",
+                f"First Activation Time [{self.time_unit}]",
                 f"Current [{self.trace_unit}]",
             ],
             trace_unit=self.trace_unit,
